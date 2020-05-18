2d5a390c541c0b8190cb6c6bfe3d921d85013e75
Erwin <enavarrete2010@gmail.com>
2020-05-18 16:12:40 -0400

    add get user

---
+++ domain/users/user_dao.go
@@ -0,0 +1,29 @@
+package users
+
+
+import (
+	"github.com/rewin23/bookstore-apiuser/utils/errors"
+	"fmt"
+) 
+
+var (
+	usersDB = make(map[int64]*User)
+)
+
+func (user *User) Get() *errors.RestErr {
+	result := usersDB[user.Id]
+	if result == nil {
+		return errors.NewNotFoundErr(fmt.Sprintf("user %d not found", user.Id))
+	}
+
+	user.Id = result.Id
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+	return nil
+}
+
+func (user User ) Save() *errors.RestErr {
+	return nil
+}
